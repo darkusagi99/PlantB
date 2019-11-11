cd143683fa1cfdd8d421c4a6a3fb0068740d1657
usagi99 <darkusagi99@gmail.com>
2019-11-11 13:46:15 +0100

    Indentation du code de la page

--- src/presence/presencelist.js
+++ src/presence/presencelist.js
@@ -5,110 +5,123 @@ import firebase from '../firebase';
 
 class Presence extends Component {
 
-        constructor(props) {
-          super(props);
-          this.displayFormatedDate = this.displayFormatedDate.bind(this);
-          this.displayFormatedTime = this.displayFormatedTime.bind(this);
+    // Constructeur
+    constructor(props) {
+        super(props);
 
-          this.peopleRef = firebase.firestore().collection('peoples');
-          this.presenceRef = firebase.firestore().collection('presences');
+        // Bind des méthodes
+        this.displayFormatedDate = this.displayFormatedDate.bind(this);
+        this.displayFormatedTime = this.displayFormatedTime.bind(this);
 
-          this.state = { presences: [],
-                        peoples: []};
-        }
+        // Initialisations firebase
+        this.peopleRef = firebase.firestore().collection('peoples');
+        this.presenceRef = firebase.firestore().collection('presences');
 
-        componentDidMount() {
+        // Initialisation state
+        this.state = {
+            presences: [],
+            peoples: []
+        };
+    }
 
-            var newPresence = [];
-            var newPeople = [];
-            var that = this;
+    // Méthodes pour le chargement des présences
+    componentDidMount() {
 
-            this.presenceRef.get()
-            .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    // doc.data() is never undefined for query doc snapshots
-                    var currentData = doc.data();
-                    currentData.id = doc.id;
+        var newPresence = [];
+        var newPeople = [];
+        var that = this;
 
-                    newPresence.push(currentData);
+        // Chargement des présences
+        this.presenceRef.get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                var currentData = doc.data();
+                currentData.id = doc.id;
 
-                    that.setState({
-                        presences : newPresence
-                    });
+                newPresence.push(currentData);
 
-                    console.log(doc.id, " => ", doc.data());
+                that.setState({
+                    presences : newPresence
                 });
+
+                console.log(doc.id, " => ", doc.data());
             });
+        });
 
-            this.peopleRef.get()
-            .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                    // doc.data() is never undefined for query doc snapshots
-                    var currentData = doc.data();
-                    currentData.id = doc.id;
+        // Chargement des personnes
+        this.peopleRef.get()
+        .then(function(querySnapshot) {
+            querySnapshot.forEach(function(doc) {
+                // doc.data() is never undefined for query doc snapshots
+                var currentData = doc.data();
+                currentData.id = doc.id;
 
-                    newPeople.push(currentData);
+                newPeople.push(currentData);
 
-                    that.setState({
-                        peoples: newPeople
-                    });
-
-                    console.log(doc.id, " => ", doc.data());
+                that.setState({
+                    peoples: newPeople
                 });
+
+                console.log(doc.id, " => ", doc.data());
             });
+        });
 
-        }
+    }
 
-        displayFormatedTime(date) {
-            var dateToFormat = new Date(date.seconds*1000)
-            return dateToFormat.getHours() + ":" + dateToFormat.getMinutes().toString().padStart(2,0);
-        }
+    // Affichage de la date formattée
+    displayFormatedTime(date) {
+        var dateToFormat = new Date(date.seconds*1000)
+        return dateToFormat.getHours() + ":" + dateToFormat.getMinutes().toString().padStart(2,0);
+    }
 
-        displayFormatedDate(date) {
-            var dateToFormat = new Date(date.seconds*1000)
-            return dateToFormat.toLocaleDateString();
-        }
+    // Affichage de l'heure formattée
+    displayFormatedDate(date) {
+        var dateToFormat = new Date(date.seconds*1000)
+        return dateToFormat.toLocaleDateString();
+    }
 
-        render() {
-            return (
+    // Rendu de la page
+    render() {
+        return (
+            <div>
+                <center><h1>Présences</h1></center>
+
                 <div>
-                    <center><h1>Présences</h1></center>
-
-                    <div>
-                        <Link to={'/presence/create'} className="nav-link">Création Présence</Link>
-                    </div>
-
-                     <table class="table">
-                     <thead>
-                         <tr>
-                               <th scope="col">Nom</th>
-                               <th scope="col">Jour</th>
-                               <th scope="col">Arrivé</th>
-                               <th scope="col">Départ</th>
-                               <th scope="col">Repas</th>
-                               <th scope="col">&nbsp;</th>
-                         </tr>
-                     </thead>
-                     <tbody>
-                     {this.state.presences.map((presence) => (
-                         <tr key={presence.id}>
-                             <td>{this.state.peoples.filter((people) => (people.id == presence.personId)).map((people) => people.fullname)}</td>
-                             <td>{presence.presenceDay ? (this.displayFormatedDate(presence.presenceDay)) : ("-")}</td>
-                             <td>{presence.arrival ? (this.displayFormatedTime(presence.arrival)) : ("-")}</td>
-                             <td>{presence.departure ? (this.displayFormatedTime(presence.departure)) : ("-")}</td>
-                             <td>{presence.hasMeal ? ("Avec Repas") : ("Sans Repas")}</td>
-
-                             <td><Link to={'/presence/update/' + presence.id} className="nav-link">MàJ Présence</Link></td>
-                         </tr>
-                     ))}
-                     </tbody>
-                     </table>
-
-
+                    <Link to={'/presence/create'} className="nav-link">Création Présence</Link>
                 </div>
-            )
-        }
-     };
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nom</th>
+                            <th scope="col">Jour</th>
+                            <th scope="col">Arrivé</th>
+                            <th scope="col">Départ</th>
+                            <th scope="col">Repas</th>
+                            <th scope="col">&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.state.presences.map((presence) => (
+                            <tr key={presence.id}>
+                                <td>{this.state.peoples.filter((people) => (people.id == presence.personId)).map((people) => people.fullname)}</td>
+                                <td>{presence.presenceDay ? (this.displayFormatedDate(presence.presenceDay)) : ("-")}</td>
+                                <td>{presence.arrival ? (this.displayFormatedTime(presence.arrival)) : ("-")}</td>
+                                <td>{presence.departure ? (this.displayFormatedTime(presence.departure)) : ("-")}</td>
+                                <td>{presence.hasMeal ? ("Avec Repas") : ("Sans Repas")}</td>
+
+                                <td><Link to={'/presence/update/' + presence.id} className="nav-link">MàJ Présence</Link></td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+
+            </div>
+        )
+    }
+
+};
 
 
-    export default Presence
+export default Presence
